1717acff7add0d440de03edb2a4e337035c8dc3e
Nick517 <63612816+Nick517@users.noreply.github.com>
2023-06-16 19:29:52 -0700

    Made ChunkLoader store tuples
    Made it so that ChunkLoader stores the position, size, and chunk as a list of tuples, instead of just storing chunks and checking their positions and sizes directly.

--- Assets/Code/ChunkLoader.cs
+++ Assets/Code/ChunkLoader.cs
@@ -15,13 +15,13 @@ public class ChunkLoader : MonoBehaviour
     public bool trackSceneView = false;
 
     private float ChunkSize => chunkCubeCount * marchingCubeChunkPrefab.cubeSize;
-    private Vector3 lastChunk = new(-1, 0, 0);
+    private Vector3 _lastChunk = new(-1, 0, 0);
 
-    private readonly List<MarchingCubes> chunks = new();
+    private readonly List<Tuple<Vector3, float, MarchingCubes>> _chunks = new();
 
     private void Update()
     {
-        Vector3 currentChunk = lastChunk;
+        Vector3 currentChunk = _lastChunk;
 
         if (trackSceneView)
         {
@@ -37,16 +37,16 @@ public class ChunkLoader : MonoBehaviour
             currentChunk = GetCurrentChunk(trackPoint.position);
         }
 
-        if (currentChunk != lastChunk)
+        if (currentChunk != _lastChunk)
         {
             SpawnChunks(currentChunk);
-            lastChunk = currentChunk;
+            _lastChunk = currentChunk;
         }
     }
 
     private void SpawnChunks(Vector3 origin)
     {
-        List<MarchingCubes> chunks = new();
+        List<Tuple<Vector3, float, MarchingCubes>> newChunks = new();
 
         int chunkScale = 1;
 
@@ -64,7 +64,7 @@ public class ChunkLoader : MonoBehaviour
                             position *= ChunkSize * chunkScale;
                             position += ChunkSize * origin;
 
-                            chunks.Add(CreateChunk(position, chunkScale));
+                            newChunks.Add(new(position, chunkScale, null));
                         }
                     }
                 }
@@ -73,7 +73,34 @@ public class ChunkLoader : MonoBehaviour
             chunkScale *= 2;
         }
 
-        LoadChunks(chunks);
+        LoadChunks(newChunks);
+    }
+
+    public void LoadChunks(List<Tuple<Vector3, float, MarchingCubes>> newChunks)
+    {
+        ChunkDataEqualityComparer comparer = new();
+
+        List<Tuple<Vector3, float, MarchingCubes>> chunksToDestroy = _chunks.Except(newChunks, comparer).ToList();
+        chunksToDestroy.ForEach(chunk => { if (chunk.Item3 != null) { Destroy(chunk.Item3.gameObject); } });
+        _ = _chunks.RemoveAll(chunk => chunksToDestroy.Contains(chunk, comparer));
+
+        List<Tuple<Vector3, float, MarchingCubes>> chunksToCreate = newChunks.Except(_chunks, comparer).ToList();
+        List<Tuple<Vector3, float, MarchingCubes>> createdChunks = new();
+        chunksToCreate.ForEach(chunk => { createdChunks.Add(new(chunk.Item1, chunk.Item2, CreateChunk(chunk.Item1, chunk.Item2))); });
+        _chunks.AddRange(createdChunks);
+    }
+
+    private class ChunkDataEqualityComparer : IEqualityComparer<Tuple<Vector3, float, MarchingCubes>>
+    {
+        public bool Equals(Tuple<Vector3, float, MarchingCubes> chunk1, Tuple<Vector3, float, MarchingCubes> chunk2)
+        {
+            return chunk1.Item1.Equals(chunk2.Item1) && chunk1.Item2.Equals(chunk2.Item2);
+        }
+
+        public int GetHashCode(Tuple<Vector3, float, MarchingCubes> chunk)
+        {
+            return chunk.Item1.GetHashCode() ^ chunk.Item2.GetHashCode();
+        }
     }
 
     private MarchingCubes CreateChunk(Vector3 position, float cubeSize)
@@ -81,25 +108,10 @@ public class ChunkLoader : MonoBehaviour
         MarchingCubes chunk = Instantiate(marchingCubeChunkPrefab, position, Quaternion.identity, transform);
         chunk.name = $"Chunk {position}, size: {cubeSize}";
         chunk.cubeSize *= cubeSize;
+        chunk.cubeMap = TerrainGenerator.PopulateMap(chunkCubeCount, cubeSize, position);
+        chunk.CreateMeshData();
 
         return chunk;
-    }
-
-    public void LoadChunks(List<MarchingCubes> newChunks)
-    {
-        List<MarchingCubes> chunksToDestroy = chunks.Except(newChunks).ToList();
-        chunksToDestroy.ForEach(thing => Destroy(thing.gameObject));
-        _ = chunks.RemoveAll(chunk => chunksToDestroy.Contains(chunk));
-
-        List<MarchingCubes> chunksToCreate = newChunks.Except(chunks).ToList();
-        chunks.AddRange(chunksToCreate);
-        chunksToCreate.ForEach(chunk => UpdateChunk(chunk));
-    }
-
-    private void UpdateChunk(MarchingCubes chunk)
-    {
-        chunk.cubeMap = TerrainGenerator.PopulateMap(chunkCubeCount, chunk.cubeSize, chunk.transform.position);
-        chunk.CreateMeshData();
     }
 
     public Vector3 GetCurrentChunk(Vector3 position)
