1ee01a7ab5ce834f67b7a8e33104b721758dca45
Nick517 <63612816+Nick517@users.noreply.github.com>
2025-08-16 18:55:15 -0700

    CreateRenderMeshJob Optimization
    Optimized CreateRenderMeshJob so that it could query for existing vertexes at O(1) by using arrays. Also made it so that the cube size is calculated only once.

--- Assets/Scripts/ECS/Jobs/TerrainGeneration/Renderer/CreateRendererMeshJob.cs
+++ Assets/Scripts/ECS/Jobs/TerrainGeneration/Renderer/CreateRendererMeshJob.cs
@@ -1,3 +1,4 @@
+using System;
 using DataTypes;
 using DataTypes.Trees;
 using ECS.BufferElements.TerrainGeneration.Renderer;
@@ -32,7 +33,6 @@ namespace ECS.Jobs.TerrainGeneration.Renderer
             var segmentInfos = chunk.GetNativeArray(ref SegmentInfoTypeHandle);
 
             var enumerator = new ChunkEntityEnumerator(useEnabledMask, chunkEnabledMask, chunk.Count);
-
             var processed = 0;
 
             while (enumerator.NextEntityIndex(out var i))
@@ -42,9 +42,10 @@ namespace ECS.Jobs.TerrainGeneration.Renderer
                 var entity = entities[i];
                 var pos = segmentInfos[i].Position;
                 var scale = segmentInfos[i].Scale;
+                var cubeSize = GetCubeSize(Settings.BaseCubeSize, scale);
 
-                var vertexes = new NativeList<float3>(Allocator.Temp);
                 var cubeMap = PopulateMap(Settings, TgTreeBlob, Maps, pos, scale);
+                var vertexes = new Vertexes(Settings.CubeCount);
 
                 Ecb.AddBuffer<TriangleBufferElement>(i, entity);
                 Ecb.AddBuffer<VertexBufferElement>(i, entity);
@@ -52,29 +53,28 @@ namespace ECS.Jobs.TerrainGeneration.Renderer
                 for (var x = 0; x < Settings.CubeCount; x++)
                 for (var y = 0; y < Settings.CubeCount; y++)
                 for (var z = 0; z < Settings.CubeCount; z++)
-                    MarchCube(i, Ecb, Settings, entity, scale, cubeMap, new int3(x, y, z), ref vertexes);
+                    MarchCube(i, Ecb, Settings, entity, cubeSize, cubeMap, new int3(x, y, z), ref vertexes);
 
-                foreach (var vert in vertexes) Ecb.AppendToBuffer<VertexBufferElement>(i, entity, vert);
-                
+                foreach (var vert in vertexes.Positions)
+                    Ecb.AppendToBuffer<VertexBufferElement>(i, entity, vert);
+
                 Ecb.AddComponent(i, entity, LocalTransform.FromPosition(pos));
                 Ecb.RemoveComponent<CreateRendererMeshTag>(i, entity);
                 Ecb.AddComponent<SetRendererMeshTag>(i, entity);
 
+                vertexes.Dispose();
                 processed++;
             }
         }
 
         private static void MarchCube(int sortKey, EntityCommandBuffer.ParallelWriter ecb, BaseSegmentSettings settings,
-            Entity entity, int scale, CubicArray<float> cubeMap, int3 position, ref NativeList<float3> vertexes)
+            Entity entity, float cubeSize, CubicArray<float> cubeMap, int3 position, ref Vertexes vertexes)
         {
-            var cubeSize = GetCubeSize(settings.BaseCubeSize, scale);
-
             var cube = new NativeArray<float>(8, Allocator.Temp);
 
             for (var i = 0; i < 8; i++) cube[i] = cubeMap.GetAt(position + Corner(i));
 
             var config = GetCubeConfig(settings, cube);
-
             if (config is 0 or 255) return;
 
             var edgeIndex = 0;
@@ -82,13 +82,12 @@ namespace ECS.Jobs.TerrainGeneration.Renderer
             for (var i = 0; i < 15; i++)
             {
                 var index = Triangle(config, edgeIndex);
-
                 if (index == -1) return;
 
                 var vertPos = IndexToVertexPosition(settings, position, ref cube, index);
-                var vert = VertexToIndex(vertPos * cubeSize, ref vertexes);
+                var vertIndex = vertexes.GetOrCreateVertex(position, index, vertPos * cubeSize);
 
-                ecb.AppendToBuffer<TriangleBufferElement>(sortKey, entity, vert);
+                ecb.AppendToBuffer<TriangleBufferElement>(sortKey, entity, vertIndex);
 
                 edgeIndex++;
             }
@@ -105,17 +104,6 @@ namespace ECS.Jobs.TerrainGeneration.Renderer
             return configIndex;
         }
 
-        private static int VertexToIndex(float3 vertex, ref NativeList<float3> vertexes)
-        {
-            for (var i = 0; i < vertexes.Length; i++)
-                if (vertexes[i].Equals(vertex))
-                    return i;
-
-            vertexes.Add(vertex);
-
-            return vertexes.Length - 1;
-        }
-
         private static float3 IndexToVertexPosition(BaseSegmentSettings settings, int3 position,
             ref NativeArray<float> cube, int index)
         {
@@ -126,10 +114,66 @@ namespace ECS.Jobs.TerrainGeneration.Renderer
             var sample2 = cube[Edge(index, 1)];
 
             var dif = sample2 - sample1;
-
             dif = dif == 0 ? settings.MapSurface : (settings.MapSurface - sample1) / dif;
 
             return vert1 + (float3)(vert2 - vert1) * dif;
+        }
+
+        private struct Vertexes : IDisposable
+        {
+            public NativeList<float3> Positions;
+
+            private NativeArray<int> _vertIndices;
+            private readonly int _vertLength;
+
+            public Vertexes(int cubeCount)
+            {
+                _vertLength = cubeCount + 1;
+
+                Positions = new NativeList<float3>(Allocator.Temp);
+                _vertIndices = new NativeArray<int>(_vertLength * _vertLength * _vertLength * 3, Allocator.Temp);
+            }
+
+            public int GetOrCreateVertex(int3 cubeIndex, int edgeIndex, float3 position)
+            {
+                var flatIndex = GetFlatIndex(cubeIndex, edgeIndex);
+
+                var index = _vertIndices[flatIndex];
+                if (index != 0) return index - 1;
+
+                index = Positions.Length;
+                Positions.Add(position);
+
+                _vertIndices[flatIndex] = index + 1;
+
+                return index;
+            }
+
+            private int GetFlatIndex(int3 index3D, int edgeIndex)
+            {
+                var dimension = EdgeToDimension[edgeIndex];
+                index3D += EdgeOffset[edgeIndex];
+
+                return index3D.x +
+                       index3D.y * _vertLength +
+                       index3D.z * _vertLength * _vertLength +
+                       dimension * _vertLength * _vertLength * _vertLength;
+            }
+
+            private static readonly int[] EdgeToDimension = { 0, 1, 0, 1, 0, 1, 0, 1, 2, 2, 2, 2 };
+
+            private static readonly int3[] EdgeOffset =
+            {
+                new(0, 0, 0), new(1, 0, 0), new(0, 1, 0), new(0, 0, 0),
+                new(0, 0, 1), new(1, 0, 1), new(0, 1, 1), new(0, 0, 1),
+                new(0, 0, 0), new(1, 0, 0), new(1, 1, 0), new(0, 1, 0)
+            };
+
+            public void Dispose()
+            {
+                if (_vertIndices.IsCreated) _vertIndices.Dispose();
+                if (Positions.IsCreated) Positions.Dispose();
+            }
         }
     }
 }
